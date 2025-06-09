58aa2aa375032cd582c442a9f48c10439c2bf257
348662958 <348662958@gapps.yrdsb.ca>
2025-06-09 12:35:43 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
-
+import java.util.Scanner;
+import java.util.ArrayList();
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> s_name = new ArrayList<String>();
+        System.out.println("Would you like to add or book a service");
+        String service = sc.nextLine().toUpperCase();
+        
+        if(service.equals("ADD")){
+            boolean add = true; //to ensure that the user woud like to add more services
+            while(true){
+                System.out.print("Enter service name: ");
+                s_name.add(sc.nextLine());
+                System.out.print("Enter category: ");
+                s_name.add(sc.nextLine());
+            }
+        }
+        else{
