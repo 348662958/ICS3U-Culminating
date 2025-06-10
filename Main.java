b27f58ef23b203e59990afc44c3d01b10dbc8364
348662958 <348662958@gapps.yrdsb.ca>
2025-06-10 10:40:14 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -4,16 +4,34 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> s_name = new ArrayList<String>();
-        System.out.println("Would you like to add or book a service");
+        ArrayList<String> c_name = new ArrayList<String>();
+        ArrayList<Double> rate = new ArrayList<Double>();
+        System.out.print("Would you like to add or book a service? ");
         String service = sc.nextLine().toUpperCase();
         
-        if(service.equals("ADD")){
+        if(service.contains("ADD")){
             boolean add = true; //to ensure that the user woud like to add more services
-            while(true){
+            while(add){ //allow for looping if the user would like
                 System.out.print("Enter service name: ");
                 s_name.add(sc.nextLine());
                 System.out.print("Enter category: ");
-                s_name.add(sc.nextLine());
+                c_name.add(sc.nextLine());
+                System.out.print("Enter cost (DO NOT enter \"$\"): ");
+                rate.add(sc.nextdouble());
+                
+                System.out.print("Would you like to add another service (Y/N)? ");
+                switch(sc.nextLine().toUpperCase()){
+                    case "Y" -> add = true; //the loop will contiune
+                    case "N" -> add = false; //the loop will end
+                }
             }
+            for(int i = 1; i < s_name.length(); i++){
+                System.out.print("You have added "+ s_name(i) + " under the " + c_name(i) + " category for " + rate(i));
+            }
+            
+        } else {
+            
         }
-        else{
+        
+    }
+}
