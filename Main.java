a010b7e0c8278a609329b41a38e95148b88736a6
348662958 <348662958@gapps.yrdsb.ca>
2025-06-12 10:57:54 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -5,38 +5,40 @@ import java.util.Arrays;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> service = new ArrayList<String>();
-        ArrayList<String> cart = new ArrayList<String>();
+        ArrayList<String> s_name = new ArrayList<String>();
+        ArrayList<String> s_categories = new ArrayList<String>();
+        ArrayList<Double> s_rate = new ArrayList<Double>();
+        ArrayList<Boolean> s_booked = new ArrayList<>();
+        
+        ArrayList<Integer> cart = new ArrayList<>();
         String[] categories = {"Tutoirng", "Plumbing", "Cleaning"};
         
-        boolean repeat = true;
-        while (repeat){
-            System.out.print("Would you like to add or book a service? ");
-            String service = sc.nextLine().toUpperCase();
-        
-            if(service.contains("ADD")){
-                boolean add = true; //to ensure that the user woud like to add more services
-                while(add){ //allow for looping if the user would like
-                    System.out.print("Enter service name: ");
-                    s_name.add(sc.nextLine());
-                    System.out.print("Enter category: ");
-                    c_name.add(sc.nextLine());
-                    System.out.print("Enter cost (DO NOT enter \"$\"): ");
-                    rate.add(sc.nextDouble());
-                    
-                    System.out.print("Would you like to add another service (Y/N)? ");
-                    switch(sc.nextLine().toUpperCase()){
-                        case "Y" -> add = true; //the loop will contiune
-                        case "N" -> add = false; //the loop will end
+        boolean run = true;
+        while (run){
+            System.out.print("1. Add a Service \n 2. Book a Service \n 3. View/Update Cart \n 4. Checkout \n 5. Exit \n Enter Option: ");
+            int option = sc.nextInt();
+            switch (option){
+                case 1:
+                    while(true){
+                        System.out.print("Service name: ");
+                        s_name.add(sc.nextLine());
+                        System.out.print("Category: ");
+                        s_categories.add(sc.nextLine());
+                        System.out.print("Hourly Rate: ");
+                        double rate = Double.parseDouble(sc.nextLine());
+                        s_booked.add(true);
+                        
+                        
+                        System.out.print("Your service has been added! Would you like to add another (Yes/No)? ");
+                        if(!sc.nextLine().equalsIgnoreCase("yes"))
+                            break;
                     }
+                    break;
+                    
+                case 2: {
+                    
                 }
-                for(int i = 1; i < s_name.size(); i++){
-                    System.out.print("You have added "+ s_name.get(i) + " under the " + c_name.get(i) + " category for " + rate.get(i));
             }
             
-        } else {
-            
-        }
-        
     }
 }
